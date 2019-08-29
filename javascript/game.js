728dc08c6f7bdaa2cdbe1fb05c71b8a0e6b016a9
Joshua Coleman <procoleman77@outlook.com>
2019-08-29 17:08:45 -0400

    added wins and lose alert, added values in target score

--- javascript/game.js
+++ javascript/game.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     var numberOptions = [10, 5, 3, 7];
     var imageCrystal
     var crystalNumber;
-    Wins = 0;
+    wins = 0;
     Lose = 0;
     var wincounter = 0;
     var losscounter;
@@ -20,17 +20,17 @@ $(document).ready(function () {
 
     //  Computer number random every time the game loads
 
-    var targetNumber = Math.floor(Math.random() * 12) + 1;;
+    var targetNumber = Math.floor(Math.random() * 120) + 19;;
     $("#number-to-guess").text(targetNumber);
 
 
     //  On Click Events
-    var numberOptions = [10, 5, 3, 7];
+    
 
     for (var i = 0; i < numberOptions.length; i++) {
     var random = Math.floor(Math.random() * 12) + 1;
 
-     imageCrystal = numberOptions[i];
+     imageCrystal = numberOptions.length;
 
     
 
@@ -39,19 +39,32 @@ $(document).ready(function () {
 
 
     $(".number").on("click", function () {
-        // ... we generate a random number
 
-        // ... and then dump the random number into our random-number div.
-
+        // prints the array randomly on the crystal in taget score
           $("#crystalNumber").text(imageCrystal);
 
+        //   Adds the value of the crytals to the traget number
        counter += imageCrystal;
-       console.log(counter)
        
-        //  var crystalValue = ($(this).attr("data-crystalvalue"));
-        //  crystalValue = parseInt(crystalValue);
-        
+    
+       //    Prints to the screen
+       $("#crystalNumber").text(counter);
+       
+       
+       if(counter === targetNumber){
+       
+           alert("you win");
+        console.log(counter)
+        }
+        else if (counter >= targetNumber) {
+            alert("You lose!!");
+          }
+
         })
+
+
+
+
 
 
 
